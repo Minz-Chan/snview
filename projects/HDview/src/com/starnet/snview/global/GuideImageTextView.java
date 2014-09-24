9a9bb069963708aa8562a31d770fc968c9717ef9
zhaokangk <zhaokangk@sina.com>
2014-09-24 13:01:48 +0800

    添加第一次程序启动时，新功能引导界面

---
+++ projects/HDview/src/com/starnet/snview/global/GuideImageTextView.java
@@ -0,0 +1,48 @@
+package com.starnet.snview.global;
+
+import com.starnet.snview.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class GuideImageTextView extends RelativeLayout {
+
+	private ImageView imageView;
+	private TextView textView;
+	
+	public GuideImageTextView(Context context) {
+		super(context);
+		View view = LayoutInflater.from(context).inflate(R.layout.guideactivity_self, this,true);
+		imageView = (ImageView) view.findViewById(R.id.guide_imgeview);
+		textView = (TextView) view.findViewById(R.id.guide_experiment_txt);
+	}
+	
+	public void setViewInVisible(){
+		imageView.setVisibility(View.GONE);
+		textView.setVisibility(View.GONE);
+	}
+	
+	public void setTextInVisible(){
+		textView.setVisibility(View.GONE);
+	}
+	
+	public void setBackGround(int resid){
+		imageView.setBackgroundResource(resid);
+	}
+	
+	public void setImageResource(int resid){
+		imageView.setImageResource(resid);
+	}
+	
+	public void setText(String text){
+		textView.setText(text);
+	}
+	
+	public TextView getTextView(){
+		return textView;
+	}
+}
--- projects/HDview/src/com/starnet/snview/global/SplashActivity.java
+++ projects/HDview/src/com/starnet/snview/global/SplashActivity.java
@@ -20,105 +20,108 @@ import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
+public class SplashActivity extends Activity {
+	private final int SPLASH_DISPLAY_LENGTH = 2000; // 延时5秒
 
-public class SplashActivity extends Activity{
-	private final int SPLASH_DISPLAY_LENGTH = 2000;  //延时5秒
-	
 	private SharedPreferences preferences;
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
-		//隐去标题栏
+
+		// 隐去标题栏
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		//隐去状态栏
-		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
+		// 隐去状态栏
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		setContentView(R.layout.start_activity);
-		
-		
-		GlobalApplication.getInstance().setAppName(getString(R.string.app_name));
-		
-		
+
+		GlobalApplication.getInstance()
+				.setAppName(getString(R.string.app_name));
+
 		if (!CheckWhetherFirstStart()) {
 			showMainActivity();
 		}
-		
-		//Animation animatinoGone = AnimationUtils.loadAnimation(this,R.anim.alpha_gone);
-		
-		
+
+		// Animation animatinoGone =
+		// AnimationUtils.loadAnimation(this,R.anim.alpha_gone);
+
 	}
-	
+
 	private void showMainActivity() {
-		new Handler().postDelayed(new Runnable(){
-			public void run(){
-				Intent  mainIntent = new Intent(SplashActivity.this, RealplayActivity.class);
-				
-				SplashActivity.this.startActivity(mainIntent);	// 启动MainActivity
-				SplashActivity.this.finish();					// 结束SplashActivity
+		new Handler().postDelayed(new Runnable() {
+			public void run() {
+				// 判断是否为第一次进入，如果是第一次进入时，则显示滑动页面；否则显示RealplayActivity界面
+				preferences = getSharedPreferences("count", MODE_WORLD_READABLE);
+				int count = preferences.getInt("count", 0);
+				Intent mainIntent;
+				if (count == 1) {
+					mainIntent = new Intent(SplashActivity.this,GuideActivity.class);
+				} else {
+					mainIntent = new Intent(SplashActivity.this,RealplayActivity.class);
+				}
+				SplashActivity.this.startActivity(mainIntent); // 启动MainActivity
+				SplashActivity.this.finish(); // 结束SplashActivity
 			}
 		}, SPLASH_DISPLAY_LENGTH);
 	}
-	
+
 	@SuppressLint("WorldReadableFiles")
 	@SuppressWarnings({ "deprecation", "unchecked" })
 	private boolean CheckWhetherFirstStart() {
 		boolean isFirstStart = false;
-		
-		//读取SharedPreferences中需要的数据
-		preferences = getSharedPreferences("count",MODE_WORLD_READABLE);
+
+		// 读取SharedPreferences中需要的数据
+		preferences = getSharedPreferences("count", MODE_WORLD_READABLE);
 		int count = preferences.getInt("count", 0);
-		
 		// 判断程序与第几次运行,如果是第一次运行则跳转到引导页面
 		if (count == 0) {
 			LoadDemoDataAsync loadtask = new LoadDemoDataAsync();
 			loadtask.execute();
-			
+
 			showDialog(PROGRESS_DIALOG);
-			
+
 			isFirstStart = true;
 		}
-		
+
 		Editor editor = preferences.edit();
 		editor.putInt("count", ++count);
 		editor.commit();
-		
+
 		return isFirstStart;
 	}
-	
-	
-	
-	
+
 	private static final int PROGRESS_DIALOG = 1;
-	@Override 
-	protected Dialog onCreateDialog(int id) { 
-		switch(id) { 
-		case PROGRESS_DIALOG:			
-			ProgressDialog progressDialog = ProgressDialog.show(this, "", "演示数据加载中...", true,true);
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case PROGRESS_DIALOG:
+			ProgressDialog progressDialog = ProgressDialog.show(this, "",
+					"演示数据加载中...", true, true);
 			progressDialog.setCancelable(false);
-//			progressDialog.setOnCancelListener(new OnCancelListener(){
-//				@Override
-//				public void onCancel(DialogInterface dialog) {
-//					//dismissDialog(PROGRESS_DIALOG);
-//					
-//				}
-//			});
+			// progressDialog.setOnCancelListener(new OnCancelListener(){
+			// @Override
+			// public void onCancel(DialogInterface dialog) {
+			// //dismissDialog(PROGRESS_DIALOG);
+			//
+			// }
+			// });
 			return progressDialog;
-		default: return null; 
-		} 
-	} 
-	
-	
+		default:
+			return null;
+		}
+	}
+
 	@SuppressWarnings("rawtypes")
 	private class LoadDemoDataAsync extends AsyncTask {
 
 		@Override
 		protected Object doInBackground(Object... params) {
 			File f = new File(LocalFileUtils.getLocalFileRootPath());
-			
+
 			if (!f.exists()) {
 				f.mkdir();
 			} else {
@@ -130,11 +133,15 @@ public class SplashActivity extends Activity{
 				}
 				f.mkdir();
 			}
-			
-			AssetsUtil.copyAssetsDir(SplashActivity.this, "thumbnails", LocalFileUtils.getLocalFileRootPath());
-			AssetsUtil.copyAssetsDir(SplashActivity.this, "capture", LocalFileUtils.getLocalFileRootPath());
-			AssetsUtil.copyAssetsDir(SplashActivity.this, "Picture", LocalFileUtils.getLocalFileRootPath());
-			AssetsUtil.copyAssetsDir(SplashActivity.this, "record", LocalFileUtils.getLocalFileRootPath());
+
+			AssetsUtil.copyAssetsDir(SplashActivity.this, "thumbnails",
+					LocalFileUtils.getLocalFileRootPath());
+			AssetsUtil.copyAssetsDir(SplashActivity.this, "capture",
+					LocalFileUtils.getLocalFileRootPath());
+			AssetsUtil.copyAssetsDir(SplashActivity.this, "Picture",
+					LocalFileUtils.getLocalFileRootPath());
+			AssetsUtil.copyAssetsDir(SplashActivity.this, "record",
+					LocalFileUtils.getLocalFileRootPath());
 			return null;
 		}
 
@@ -143,7 +150,7 @@ public class SplashActivity extends Activity{
 		protected void onPostExecute(Object result) {
 			SplashActivity.this.dismissDialog(PROGRESS_DIALOG);
 			showMainActivity();
-			
+
 			super.onPostExecute(result);
 		}
 
