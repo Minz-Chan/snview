8fc207b0b307c05f12112f286cdaba7c038f73a1
zhaokangk <zhaokangk@sina.com>
2015-03-18 15:03:52 +0800

    禁止加载后的界面的Toast的信息弹出（实时预览、远程回放）

--- projects/HDview/src/com/starnet/snview/images/utils/SelfDefViewPager.java
+++ projects/HDview/src/com/starnet/snview/images/utils/SelfDefViewPager.java
@@ -1,5 +1,6 @@
 package com.starnet.snview.images.utils;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
@@ -7,21 +8,22 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.TextView;
 
+@SuppressLint("Recycle")
 public class SelfDefViewPager extends ViewPager {
 
 	private final String TAG = "SelfDefViewPager";
-	boolean isScrolling = false;
-	private TextView imagepreview_title_image_num;
+//	private boolean isScrolling = false;
+	private TextView titleView;
 	private int showSum;
 
 	public SelfDefViewPager(Context context) {
 		super(context);
 		initial();
 	}
-	
-	public SelfDefViewPager(Context context,TextView imagepreview_title_image_num,int showSum) {
+
+	public SelfDefViewPager(Context context, TextView titleView, int showSum) {
 		super(context);
-		this.imagepreview_title_image_num = imagepreview_title_image_num;
+		this.titleView = titleView;
 		this.showSum = showSum;
 		initial();
 	}
@@ -38,53 +40,53 @@ public class SelfDefViewPager extends ViewPager {
 	OnPageChangeListener listener = new OnPageChangeListener() {// 重写了OnPageChangeListener的实现方法...
 
 		@Override
-		public void onPageScrollStateChanged(int arg0) {//判断滑动状态的改变...arg0={0,1,2};arg0=0，表示没有做什么；arg0=1，表示正在滑动；arg0=2，表示滑动结束；
-			if (arg0 == 1) {
-				isScrolling = true;
-			} else if (arg0 == 2) {
-				isScrolling = false;
-			}else{
-				isScrolling = false;
-			}
+		public void onPageScrollStateChanged(int arg0) {// 判断滑动状态的改变...arg0={0,1,2};arg0=0，表示没有做什么；arg0=1，表示正在滑动；arg0=2，表示滑动结束；
+//			if (arg0 == 1) {
+//				isScrolling = true;
+//			} else if (arg0 == 2) {
+//				isScrolling = false;
+//			} else {
+//				isScrolling = false;
+//			}
 		}
 
 		@Override
-		public void onPageScrolled(int arg0, float arg1, int arg2) {//当页面在滑动的时候会调用此方法，在滑动被停止之前，此方法回一直得到
-			//arg0 :当前页面，及你点击滑动的页面；arg1:当前页面偏移的百分比；arg2:当前页面偏移的像素位置
+		public void onPageScrolled(int arg0, float arg1, int arg2) {// 当页面在滑动的时候会调用此方法，在滑动被停止之前，此方法回一直得到
+			// arg0 :当前页面，及你点击滑动的页面；arg1:当前页面偏移的百分比；arg2:当前页面偏移的像素位置
 		}
 
 		@Override
 		public void onPageSelected(int position) {// 此方法是页面跳转完后得到调用;arg0是你当前选中的页面的Position
-			Log.i(TAG, "onPageSelected ----> position :" + (position+1));
-			mPosition = position+1;
-			imagepreview_title_image_num.setText("("+(mPosition)+"/"+showSum+")");
+			Log.i(TAG, "onPageSelected ----> position :" + (position + 1));
+			mPosition = position + 1;
+			titleView.setText("(" + (mPosition) + "/" + showSum + ")");
 		}
 	};
-	
-	@Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        try {
-        	Log.i(TAG, "e.x:" + ev.getX());
-            return super.onInterceptTouchEvent(ev);
-        } catch (Exception ex) {
-        	
-        	Log.i(TAG, "onInterceptTouchEvent, catch");
-            ex.printStackTrace();
-            
-            MotionEvent event = MotionEvent.obtain(ev);
-            
-            event.setAction(MotionEvent.ACTION_UP);
-            
-            event.setLocation(8, 8);
 
-            Log.i(TAG, "onInterceptTouchEvent," + getChildAt(getCurrentItem()));
-            
-            return false;
-        }
-    }
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		try {
+			Log.i(TAG, "e.x:" + ev.getX());
+			return super.onInterceptTouchEvent(ev);
+		} catch (Exception ex) {
+
+			Log.i(TAG, "onInterceptTouchEvent, catch");
+			ex.printStackTrace();
+
+			MotionEvent event = MotionEvent.obtain(ev);
+
+			event.setAction(MotionEvent.ACTION_UP);
+
+			event.setLocation(8, 8);
+
+			Log.i(TAG, "onInterceptTouchEvent," + getChildAt(getCurrentItem()));
+
+			return false;
+		}
+	}
 
 	public boolean delete_flag = false;
-	
+
 	public boolean isDelete_flag() {
 		return delete_flag;
 	}
@@ -102,7 +104,8 @@ public class SelfDefViewPager extends ViewPager {
 	}
 
 	private int mPosition;
-	public int getMPostion(){
+
+	public int getMPostion() {
 		return mPosition;
 	}
 }
