75b0492143c55d4eb1bd41ecfe568aae09e0a02c
[minz.chan] <minz.chan8@gmail.com>
2015-03-18 15:12:04 +0800

    StreamDataFormatMessageHandler相关辅助调试信息打印

--- projects/HDview/src/com/starnet/snview/protocol/message/handler/StreamDataFormatMessageHandler.java
+++ projects/HDview/src/com/starnet/snview/protocol/message/handler/StreamDataFormatMessageHandler.java
@@ -4,6 +4,8 @@ import org.apache.mina.core.session.AttributeKey;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.handler.demux.MessageHandler;
 
+import android.util.Log;
+
 import com.starnet.snview.component.h264.H264DecodeUtil;
 import com.starnet.snview.component.liveview.LiveViewItemContainer;
 import com.starnet.snview.protocol.Connection;
@@ -11,8 +13,8 @@ import com.starnet.snview.protocol.message.StreamDataFormat;
 
 public class StreamDataFormatMessageHandler implements
 		MessageHandler<StreamDataFormat> {
-//	private final AttributeKey LIVEVIEW_ITEM = new AttributeKey(Connection.class, "liveview_item");
-//	private final AttributeKey H264DECODER = new AttributeKey(Connection.class, "h264decoder");
+	private static final String TAG = "StreamDataFormatMessageHandler";
+
 	private AttributeKey CONNECTION = new AttributeKey(Connection.class, "connection");
 	private H264DecodeUtil h264;
 	
@@ -22,7 +24,7 @@ public class StreamDataFormatMessageHandler implements
 	@Override
 	public void handleMessage(IoSession session, StreamDataFormat message)
 			throws Exception {
-		
+		Log.d(TAG, "StreamDataFormat is arrived...");
 		if (connection == null) {
 			connection = (Connection) session.getAttribute(CONNECTION);
 		}
@@ -35,13 +37,14 @@ public class StreamDataFormatMessageHandler implements
 			lvContainer = connection.getLiveViewItemContainer();
 		}
 		
-		// 获取帧率
-//		lvContainer.setFramerate(message.getVideoDataFormat().getFramerate());
-		lvContainer.getVideoConfig().setFramerate(message.getVideoDataFormat().getFramerate());
 		
-		System.out.println("StreamDataFormat is arrived...");
 		int width = message.getVideoDataFormat().getWidth();
 		int height = message.getVideoDataFormat().getHeight();
+		int framerate = message.getVideoDataFormat().getFramerate();
+		
+		Log.d(TAG, "width:" + width + ", height:" + height + ", fps:" + framerate);
+		
+		lvContainer.getVideoConfig().setFramerate(framerate);
 		
 		if (width > 0 && height > 0) {
 			lvContainer.getVideoConfig().setWidth(width);
@@ -54,7 +57,6 @@ public class StreamDataFormatMessageHandler implements
 			if (connection.isValid()) {
 				lvContainer.setWindowInfoContent(checkResolution(width, height));
 			}
-			
 		}
 		
 	}
