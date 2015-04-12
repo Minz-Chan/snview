760af3afd48d636736895920e88fba5a76082bac
zhaokang <1063995011@qq.com>
2015-04-12 10:56:23 +0800

    报警推送中，报警通知设置中的滑动设置/点击设置，尚未对报警推送账户设置添加滑动/点击事件

--- projects/HDview/src/com/starnet/snview/syssetting/AalarmNotifyAdapter.java
+++ projects/HDview/src/com/starnet/snview/syssetting/AalarmNotifyAdapter.java
@@ -2,21 +2,28 @@ package com.starnet.snview.syssetting;
 
 import java.util.HashMap;
 import java.util.List;
-
 import com.starnet.snview.R;
 import com.starnet.snview.component.SnapshotSound;
-
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Vibrator;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+@SuppressLint("ClickableViewAccessibility")
 public class AalarmNotifyAdapter extends BaseAdapter {
+
+	private final String TAG = "AalarmNotifyAdapter";
 
 	boolean isShake;
 	boolean isSound;
@@ -29,6 +36,8 @@ public class AalarmNotifyAdapter extends BaseAdapter {
 	private boolean isClickFlagSha;
 	private boolean isClickFlagSou;
 	private List<HashMap<String, Object>> mData;
+
+	private GestureDetector mGestureDetector;
 
 	public AalarmNotifyAdapter(Context ctx,
 			List<HashMap<String, Object>> mData, boolean isAllAcc,
@@ -45,6 +54,9 @@ public class AalarmNotifyAdapter extends BaseAdapter {
 		vibrator = (Vibrator) ctx.getSystemService(Context.VIBRATOR_SERVICE);
 		flater = (LayoutInflater) ctx
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		gestureListener = new FlipOnGestureListener();
+		mGestureDetector = new GestureDetector(ctx, gestureListener);
 	}
 
 	@Override
@@ -62,6 +74,10 @@ public class AalarmNotifyAdapter extends BaseAdapter {
 		return position;
 	}
 
+	private int curPostion;
+	private TextView curTXT;
+	private ImageButton curImgBtn;
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		if (convertView == null) {
@@ -77,7 +93,8 @@ public class AalarmNotifyAdapter extends BaseAdapter {
 		String content = map.get("text").toString();
 		txt.setText("" + content);
 
-		final TextView cnt = (TextView) convertView.findViewById(R.id.alarm_cnt);
+		final TextView cnt = (TextView) convertView
+				.findViewById(R.id.alarm_cnt);
 
 		final int pos = position;
 
@@ -107,50 +124,60 @@ public class AalarmNotifyAdapter extends BaseAdapter {
 			}
 		}
 
+		imgBtn.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				curTXT = cnt;
+				curPostion = pos;
+				curImgBtn = imgBtn;
+				return mGestureDetector.onTouchEvent(event);
+			}
+		});
+
 		imgBtn.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				if (pos == 0) {
-					if (isClickFlagAcc) {
-						isClickFlagAcc = false;
-						cnt.setText(ctx.getString(R.string.notify_accept_off));
-						imgBtn.setBackgroundResource(R.drawable.pushset_notify_off);
-					} else {
-						isClickFlagAcc = true;
-						cnt.setText(ctx.getString(R.string.notify_accept_open));
-						imgBtn.setBackgroundResource(R.drawable.pushset_notify_open);
-					}
-				} else if (pos == 1) {
-					if (isClickFlagSha) {
-						isClickFlagSha = false;
-						cnt.setText(ctx.getString(R.string.remind_shake_off));
-						imgBtn.setBackgroundResource(R.drawable.pushset_notify_off);
-					} else {
-						isClickFlagSha = true;
-						long[] pattern = { 50, 200, 50, 200 };
-						vibrator.vibrate(pattern, -1);
-						cnt.setText(ctx.getString(R.string.remind_shake_open));
-						imgBtn.setBackgroundResource(R.drawable.pushset_notify_open);
-					}
-				} else if (pos == 2) {
-					if (isClickFlagSou) {
-						isClickFlagSou = false;
-						cnt.setText(ctx.getString(R.string.remind_sound_off));
-						imgBtn.setBackgroundResource(R.drawable.pushset_notify_off);
-					} else {
-						isClickFlagSou = true;
-						cnt.setText(ctx.getString(R.string.remind_sound_open));
-						imgBtn.setBackgroundResource(R.drawable.pushset_notify_open);
-						new Thread(new Runnable() {
-							@Override
-							public void run() {
-								SnapshotSound s = new SnapshotSound(ctx);
-								s.playPushSetSound();
-							}
-						}).start();
-					}
-				}
+				// if (pos == 0) {
+				// if (isClickFlagAcc) {
+				// isClickFlagAcc = false;
+				// cnt.setText(ctx.getString(R.string.notify_accept_off));
+				// imgBtn.setBackgroundResource(R.drawable.pushset_notify_off);
+				// } else {
+				// isClickFlagAcc = true;
+				// cnt.setText(ctx.getString(R.string.notify_accept_open));
+				// imgBtn.setBackgroundResource(R.drawable.pushset_notify_open);
+				// }
+				// } else if (pos == 1) {
+				// if (isClickFlagSha) {
+				// isClickFlagSha = false;
+				// cnt.setText(ctx.getString(R.string.remind_shake_off));
+				// imgBtn.setBackgroundResource(R.drawable.pushset_notify_off);
+				// } else {
+				// isClickFlagSha = true;
+				// long[] pattern = { 50, 200, 50, 200 };
+				// vibrator.vibrate(pattern, -1);
+				// cnt.setText(ctx.getString(R.string.remind_shake_open));
+				// imgBtn.setBackgroundResource(R.drawable.pushset_notify_open);
+				// }
+				// } else if (pos == 2) {
+				// if (isClickFlagSou) {
+				// isClickFlagSou = false;
+				// cnt.setText(ctx.getString(R.string.remind_sound_off));
+				// imgBtn.setBackgroundResource(R.drawable.pushset_notify_off);
+				// } else {
+				// isClickFlagSou = true;
+				// cnt.setText(ctx.getString(R.string.remind_sound_open));
+				// imgBtn.setBackgroundResource(R.drawable.pushset_notify_open);
+				// new Thread(new Runnable() {
+				// @Override
+				// public void run() {
+				// SnapshotSound s = new SnapshotSound(ctx);
+				// s.playPushSetSound();
+				// }
+				// }).start();
+				// }
+				// }
 			}
 		});
 		return convertView;
@@ -178,5 +205,91 @@ public class AalarmNotifyAdapter extends BaseAdapter {
 
 	public void setClickFlagSou(boolean isClickFlagSou) {
 		this.isClickFlagSou = isClickFlagSou;
+	}
+
+	private FlipOnGestureListener gestureListener;
+
+	private final class FlipOnGestureListener implements OnGestureListener {
+
+		@Override
+		public boolean onDown(MotionEvent e) {
+			Log.i(TAG, "====onDown====");
+			if (curPostion == 0) {
+				if (isClickFlagAcc) {
+					isClickFlagAcc = false;
+					curTXT.setText(ctx.getString(R.string.notify_accept_off));
+					curImgBtn
+							.setBackgroundResource(R.drawable.pushset_notify_off);
+				} else {
+					isClickFlagAcc = true;
+					curTXT.setText(ctx.getString(R.string.notify_accept_open));
+					curImgBtn.setBackgroundResource(R.drawable.pushset_notify_open);
+				}
+			} else if (curPostion == 1) {
+				if (isClickFlagSha) {
+					isClickFlagSha = false;
+					curTXT.setText(ctx.getString(R.string.remind_shake_off));
+					curImgBtn
+							.setBackgroundResource(R.drawable.pushset_notify_off);
+				} else {
+					isClickFlagSha = true;
+					long[] pattern = { 50, 200, 50, 200 };
+					vibrator.vibrate(pattern, -1);
+					curTXT.setText(ctx.getString(R.string.remind_shake_open));
+					curImgBtn
+							.setBackgroundResource(R.drawable.pushset_notify_open);
+				}
+			} else if (curPostion == 2) {
+				if (isClickFlagSou) {
+					isClickFlagSou = false;
+					curTXT.setText(ctx.getString(R.string.remind_sound_off));
+					curImgBtn
+							.setBackgroundResource(R.drawable.pushset_notify_off);
+				} else {
+					isClickFlagSou = true;
+					curTXT.setText(ctx.getString(R.string.remind_sound_open));
+					curImgBtn
+							.setBackgroundResource(R.drawable.pushset_notify_open);
+					new Thread(new Runnable() {
+						@Override
+						public void run() {
+							SnapshotSound s = new SnapshotSound(ctx);
+							s.playPushSetSound();
+						}
+					}).start();
+				}
+			}
+			return false;
+		}
+
+		@Override
+		public void onShowPress(MotionEvent e) {
+
+		}
+
+		@Override
+		public boolean onSingleTapUp(MotionEvent e) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2,
+				float distanceX, float distanceY) {
+			Log.i(TAG, "====onScroll====");
+			return false;
+		}
+
+		@Override
+		public void onLongPress(MotionEvent e) {
+			Log.i(TAG, "====onLongPress====");
+		}
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			Log.i(TAG, "====onFling====");
+			return false;
+		}
 	}
 }
