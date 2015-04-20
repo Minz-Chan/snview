04a5d008ae1d2991229fb5efa4e371a260843297
[minz.chan] <minz.chan8@gmail.com>
2015-04-20 22:40:40 +0800

    [变更]实时预览设备登录返回LoginResponse按协议逻辑进行处理; [变更]远程回放中提示“当前为录像状态，请先停止录像” => “当前为剪辑状态，请先停止剪辑”; [变更]侧滑菜单“用户名” => “SNview”.

--- projects/HDview/src/com/starnet/snview/protocol/message/handler/LoginResponseMessageHandler.java
+++ projects/HDview/src/com/starnet/snview/protocol/message/handler/LoginResponseMessageHandler.java
@@ -36,24 +36,44 @@ public class LoginResponseMessageHandler implements
 			lvContainer.setWindowInfoContent(R.string.connection_response_login_success);
 			break;
 		case Constants.RESPONSE_CODE._RESPONSECODE_USER_PWD_ERROR:		// 用户名或密码错
+			lvContainer.setWindowInfoContent(R.string.connection_response_user_pwd_error);
+			lvContainer.setIsResponseError(true);
 			break;
 		case Constants.RESPONSE_CODE._RESPONSECODE_PDA_VERSION_ERROR:	// 版本不一致
+			lvContainer.setWindowInfoContent(R.string.connection_response_pda_version_error);
+			lvContainer.setIsResponseError(true);
 			break;
 		case Constants.RESPONSE_CODE._RESPONSECODE_MAX_USER_ERROR:	    // 已达最大用户数
+			lvContainer.setWindowInfoContent(R.string.connection_response_max_user_error);
+			lvContainer.setIsResponseError(true);
 			break;
 		case Constants.RESPONSE_CODE._RESPONSECODE_DEVICE_OFFLINE:		// 设备已经离线
+			lvContainer.setWindowInfoContent(R.string.connection_response_device_offline);
+			lvContainer.setIsResponseError(true);
 			break;
 		case Constants.RESPONSE_CODE._RESPONSECODE_DEVICE_HAS_EXIST:	// 设备已经存在
+			lvContainer.setWindowInfoContent(R.string.connection_response_device_has_exist);
+			lvContainer.setIsResponseError(true);
 			break;
 		case Constants.RESPONSE_CODE._RESPONSECODE_DEVICE_OVERLOAD:		// 设备性能超载(设备忙)
+			lvContainer.setWindowInfoContent(R.string.connection_response_device_overload);
+			lvContainer.setIsResponseError(true);
 			break;
 		case Constants.RESPONSE_CODE._RESPONSECODE_INVALID_CHANNLE:		// 设备不支持的通道
+			lvContainer.setWindowInfoContent(R.string.connection_response_invalid_channel);
+			lvContainer.setIsResponseError(true);
 			break;
 		case Constants.RESPONSE_CODE._RESPONSECODE_PROTOCOL_ERROR:		// 协议解析出错
+			lvContainer.setWindowInfoContent(R.string.connection_response_protocol_error);
+			lvContainer.setIsResponseError(true);
 			break;
 		case Constants.RESPONSE_CODE._RESPONSECODE_NOT_START_ENCODE:	// 未启动编码
+			lvContainer.setWindowInfoContent(R.string.connection_response_not_start_encode);
+			lvContainer.setIsResponseError(true);
 			break;
 		case Constants.RESPONSE_CODE._RESPONSECODE_TASK_DISPOSE_ERROR:	// 任务处理过程出错
+			lvContainer.setWindowInfoContent(R.string.connection_response_task_dispose_error);
+			lvContainer.setIsResponseError(true);
 			break;
 		default: // 兼容旧版，登录服务器失败，原因即用户或密码错误
 			lvContainer.setWindowInfoContent(R.string.connection_response_user_pwd_error);
